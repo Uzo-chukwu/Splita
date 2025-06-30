7557a7dc1dcd1d19c63b630747b9ed52aa89eb5e
uzo-chukwu <israeljohnson0464@gmail.com>
2025-06-30 10:52:21 +0100

    Postgres DB + user setup, configured settings.py, ran initial migration

--- splita_backend/settings.py
+++ splita_backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'splita_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'splita_db',
+        'USER': 'splita_user',
+        'PASSWORD': 'splita_pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
